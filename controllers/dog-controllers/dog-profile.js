ec5b3d68816cb3f6ddc5a3a3e25fde9f6a931735
Ryan <ryanpvoong@gmail.com>
2023-06-11 23:11:28 -0400

    added more profile data

--- controllers/dog-controllers/dog-profile.js
+++ controllers/dog-controllers/dog-profile.js
@@ -7,9 +7,19 @@ export const dogProfile = async (req, res) => {
   }
   const fetchDogProfile = await axios.get(`https://api.petfinder.com/v2/animals/${ req.params.animalId }`, { headers: headers })
 
-  console.log('query: ', fetchDogProfile.data.animal.photos);
+  const result = []
+  Object.keys(fetchDogProfile.data.animal.environment).forEach((env) => {
+    console.log('inside: ', env);
+    if (fetchDogProfile.data.animal.environment[env] === false) {
+      result.push(env)
+    }
+  })
+
+  console.log('env: ', result.toString());
+  // console.log('query: ', fetchDogProfile.data.animal.photos);
   res.render('pages/dog-profile.ejs', {
-    data: fetchDogProfile.data.animal
+    data: fetchDogProfile.data.animal,
+    environment: result
   })
   // res.end()
 }
